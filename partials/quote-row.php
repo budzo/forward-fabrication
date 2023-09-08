b78f74f0a7b77857ccbaf81d61c50d80876841f9
Chris Budzban <chris@elexicon.com>
2023-09-08 15:38:19 -0500

    styling tweaks

--- partials/quote-row.php
+++ partials/quote-row.php
@@ -4,7 +4,7 @@
             <div class="col-lg-8 mx-auto">
                 <h2>Get a FREE Quote</h2>
                 <p>
-                    Eager to see your fabrication project take shape? Look no further than Forward Fabrication & Mechanical. Our expertise and dedication ensure your ideas become reality, crafted with precision and innovation. Request a free quote today for your fabrication project and discover the unparalleled quality and commitment that define Forward Fabrication & Mechanical. Let's bring your vision to life together!
+                    Eager to see your fabrication project take shape? Look no further than Forward Fabrication and Mechanical. Our expertise and dedication ensure your ideas become reality, crafted with precision and innovation. Request a free quote today for your fabrication project and discover the unparalleled quality and commitment that define Forward Fabrication and Mechanical. Let's bring your vision to life together!
                 </p>
                 <?php echo do_shortcode('[contact-form-7 title="Get a Quote"]'); ?>
             </div>
